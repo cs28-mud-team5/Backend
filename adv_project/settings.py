93e57e5cf7a5beed2f3912855cf0e54ab0fd47a6
SarmenSinanian <52585634+SarmenSinanian@users.noreply.github.com>
2020-04-30 15:50:56 -0700

    Update settings.py

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+    'default': dj_database_url.config(default=config('HOST'), conn_max_age=600)
 }
 
 # DATABASES = {
